e8548bf9bc92652446a6f4b38a54d6fe6a8ebbf7
jolujuan <thesameexample@outlook.es>
2024-02-20 16:10:38 +0100

    add readme

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { UsersServiceService } from '../../services/users.service.service';
 
 @Component({
   selector: 'app-footer',
@@ -10,4 +11,10 @@ import { RouterLink } from '@angular/router';
 })
 export class FooterComponent {
   currentYear: number = new Date().getFullYear();
+  isLoggedUser: boolean = false;
+
+  constructor(private usersService: UsersServiceService) {
+    this.isLoggedUser = this.usersService.isLogged();
+
+  }
 }
